d499c313d50ed10059497933005481bb8e65be8f
Tim Kocik <timothypkocik@gmail.com>
2016-08-25 07:27:57 -0400

    Added onCheckboxClicked method.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.quizapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method is called when the submit button is clicked.
+     *
      * @param view
      */
     public void submit(View view) {
@@ -31,12 +33,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onRadioButtonClicked(View view){
+    public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
 
-        // Check which radio button was clicked
-        switch(view.getId()) {
+        // Check which radio button was clicked question 1.
+        switch (view.getId()) {
             case R.id.radio_button_true1:
                 if (checked)
                     // Incorrect answer
@@ -46,12 +48,105 @@ public class MainActivity extends AppCompatActivity {
                     // Correct answer
                     break;
         }
+        // Check which radio button was clicked question 2.
+        switch (view.getId()) {
+            case R.id.radio_button_true2:
+                if (checked)
+                    // Incorrect answer
+                    break;
+            case R.id.radio_button_false2:
+                if (checked)
+                    // Correct answer
+                    break;
+        }
+        // Check which radio button was clicked question 3.
+        switch (view.getId()) {
+            case R.id.radio_button_true3:
+                if (checked)
+                    // Incorrect answer
+                    break;
+            case R.id.radio_button_false3:
+                if (checked)
+                    // Correct answer
+                    break;
+        }
+        // Check which radio button was clicked question 4.
+        switch (view.getId()) {
+            case R.id.radio_button_true4:
+                if (checked)
+                    // Correct answer
+                    break;
+            case R.id.radio_button_false4:
+                if (checked)
+                    // Incorrect answer.
+                    break;
+        }
+
+        // Check which radio button was clicked question 5.
+        switch (view.getId()) {
+            case R.id.radio_button_true5:
+                if (checked)
+                    // Incorrect answer
+                    break;
+            case R.id.radio_button_false5:
+                if (checked)
+                    // Correct answer
+                    break;
+        }
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked for question 6 answers.
+        switch (view.getId()) {
+            //Incorrect
+            case R.id.bench_press:
+                if (checked){
+
+                }
+
+                break;
+            //Correct
+            case R.id.dead_lift:
+                if (checked){
+
+                }
+
+                break;
+            //Correct
+            case R.id.t_bar_row:
+                if (checked){
+
+                }
+                break;
+            //Incorrect
+            case R.id.squats:
+                if (checked){
+
+                }
+                break;
+            //Incorrect
+            case R.id.hammer_curls:
+                if (checked){
+
+                }
+                break;
+            //Correct
+            case R.id.cable_row:
+                if (checked){
+
+                }
+                break;
+
+        }
     }
 
     /**
-     *
      * Calculates the quiz answers.
-     * @param isTrue whether user answered true.
+     *
+     * @param isTrue  whether user answered true.
      * @param isFalse whether user answered false.
      * @return grade of the quiz.
      */
